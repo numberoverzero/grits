5de34356a3ecf9ca899d1750ea6aef4eb679efc9
Joe Cross <joe.mcross@gmail.com>
2017-01-29 16:37:52 -0800

    Scaffolding

---
+++ README.rst
@@ -0,0 +1,4 @@
+grits
+~~~~~
+
+Static blog generator that creates a single page app.  Dynamic routes are possible for some hosts (not GH-Pages).
---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+from setuptools import setup
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(HERE, "README.rst")) as f:
+    README = f.read()
+
+with open(os.path.join(HERE, "CHANGELOG.rst")) as f:
+    CHANGES = f.read()
+
+
+def get_version():
+    with open(os.path.join(HERE, "grits/__init__.py")) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1])
+
+PACKAGES = [
+    "grits"
+]
+
+REQUIREMENTS = [
+    "click==6.7",
+    "ghp-import==0.5.4",
+    "Jinja2==2.9.5",
+    "texas==0.5.2"
+]
+
+if __name__ == "__main__":
+    setup(
+        name="grits",
+        version=get_version(),
+        description="Static SPA Generator",
+        long_description=README + "\n\n" + CHANGES,
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+        ],
+        author="Joe Cross",
+        author_email="joe.mcross@gmail.com",
+        url="https://github.com/numberoverzero/grits",
+        license="MIT",
+        keywords="static blog spa generator",
+        platforms="any",
+        include_package_data=True,
+        packages=PACKAGES,
+        install_requires=REQUIREMENTS,
+    )
